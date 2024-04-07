a436101716e0309aeeb99c787572e33afa9ee7b7
zdziszkee <zdziszkee@gmail.com>
2024-04-07 23:10:20 +0200

    added including dependencies to jar README

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,8 +13,17 @@ dependencies {
     implementation("com.fasterxml.jackson.core:jackson-databind:2.17.0")
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
-}
 
+}
+tasks.withType<Jar> {
+    val dependencies = configurations
+            .runtimeClasspath
+            .get()
+            .map(::zipTree) // OR .map { zipTree(it) }
+    from(dependencies)
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+
+}
 tasks.test {
     useJUnitPlatform()
 }
