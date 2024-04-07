62e83c1d967a99ff1b93e2ca495c3e7ca03aa1b3
kuchta <kacper.kuchta@here.com>
2024-04-07 21:45:26 +0200

    empty config test

--- src/test/java/com/ocado/basket/BasketSplitterTest.java
+++ src/test/java/com/ocado/basket/BasketSplitterTest.java
@@ -1,9 +1,24 @@
 package com.ocado.basket;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
 
 class BasketSplitterTest {
+    @Test
+    public void testEmptyConfig() throws URISyntaxException{
+        final BasketSplitter basketSplitter = new BasketSplitter(getAbsolutePath(getAbsolutePath("emptyConfig.json")));
 
+    }
 
+    private String getAbsolutePath(String resource) throws URISyntaxException {
+        URL url = getClass().getClassLoader().getResource(resource);
 
+        URI uri = url.toURI();
+
+        String path = uri.getPath();
+        return  path;
+    }
 }
